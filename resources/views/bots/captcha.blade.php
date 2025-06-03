5a8345b26cfec4767a9b210307053e2d86ac332e
sandr <fazanis@mail.ru>
2025-06-03 15:14:08 +0500

    fixed math type

--- resources/views/bots/captcha.blade.php
+++ resources/views/bots/captcha.blade.php
@@ -2,7 +2,7 @@
     <h2>Подтвердите что вы не бот</h2>
     <form method="post" action="{{route('send.captcha')}}" >
         @csrf
-        {!! captcha_img(attrs:['width'=>config('block-bots.width').'px']) !!}
+        {!! captcha_img('math',attrs:['width'=>config('block-bots.width').'px']) !!}
         <p><input type="text" name="captcha"></p>
         <i style="color: red;">@error('captcha')</i>
         <div class="invalid-feedback">{{ $message }}</div> @enderror
